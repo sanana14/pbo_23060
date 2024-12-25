004605e29b723ec8f285fe047c56dd6168d915c9
sanana14 <evanugapi14@gmail.com>
2024-12-25 13:18:13 +0900

    pbo 23060

---
+++ modul.py
@@ -0,0 +1,12 @@
+def tambah(pemasukan,pengeluaran):
+    return (pemasukan+pengeluaran)
+
+def kali(pemasukan,pengeluaran):
+    return (pemasukan*pengeluaran)
+
+def kurang(pemasukan,pengeluaran):
+    return (pemasukan-pengeluaran)
+
+def bagi(pemasukan,pengeluaran):
+    return (pemasukan/pengeluaran)
+  
